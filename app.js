31378fd6e13240ecf25b662b33032ebb8f20b06f
Andreas Beasley <andreas@sapioweb.com>
2017-11-27 07:07:57 -0500

    cart removed from app and moved into package

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ angular.module('hairislifelv', [
   'ui.router',
   'ngResource',
   'LocalStorageModule',
-  'angular.filter'
+  'angular.filter',
+  'sapioCart'
 ])
 .constant('__env', env)
 
@@ -93,9 +94,6 @@ angular.module('hairislifelv', [
 
 // Config
 .config(config)
-
-// Services
-.service('cartService', cartService)
 
 // Directives
 // .directive('cart', function() {
@@ -211,150 +209,6 @@ angular.module('hairislifelv', [
     }
   }
 })
-
-// Custom Cart
-.directive('cartCount', function(localStorageService) {
-  return {
-    link: function(scope, element, attrs) {
-      var count = 0;
-
-      if(localStorageService.get('cart')) {
-        count = localStorageService.get('cart').items.length;
-      }
-
-      element.text(count);
-
-      scope.$on("LocalStorageModule.notification.setitem", function (key, newVal, type) {
-        element.text(JSON.parse(newVal.newvalue).items.length);
-      });
-    }
-  }
-})
-.directive('cartTotal', function(localStorageService) {
-  return {
-    link: function(scope, element, attrs) {
-      var total = 0;
-
-      if (localStorageService.get('cart')) {
-        total = localStorageService.get('cart').total;
-      }
-
-      element.text(total);
-
-      scope.$on("LocalStorageModule.notification.setitem", function (key, newVal, type) {
-        element.text(JSON.parse(newVal.newvalue).total);
-      });
-    }
-  }
-})
-.directive('addToCart', function(localStorageService) {
-  return {
-    link: function(scope, element, attrs) {
-      var cart = {
-        items: [],
-        count: 0,
-        total: 0,
-      };
-
-      element.bind('click', function() {
-        var updatedCart = updateCart(localStorageService.get('cart') || cart, {
-          id: attrs.productId,
-          price: attrs.productPrice
-        });
-
-        localStorageService.set('cart', updatedCart);
-      });
-    }
-  }
-})
-.directive('removeItem', function(localStorageService) {
-  return {
-    link: function(scope, element, attrs) {
-      element.bind('click', function() {
-        var updatedCart = removeFromCart(localStorageService.get('cart'), {
-          index: attrs.index,
-        });
-
-        localStorageService.set('cart', updatedCart);
-      });
-    }
-  }
-})
-
-
-
-
-
-
-
-function cartService(localStorageService, $rootScope) {
-  var items = [],
-    count = 0,
-    total = 0;
-
-  // if (localStorageService.get('cart')) {
-  //   count = localStorageService.get('cart').items.length;
-  //   items = localStorageService.get('cart').items;
-  // }
-
-  this.count = function() {
-    return count;
-  }
-
-  this.total = function() {
-    return total;
-  }
-
-  this.items = function() {
-    return items;
-  }
-
-  this.summary = function() {
-    return {
-      items: items,
-      count: count,
-      total: total
-    };
-  }
-
-  // var items = [];
-  //
-  // if (localStorageService.get('cart')) {
-  //   items = localStorageService.get('cart').items;
-  // }
-  //
-  // return items;
-}
-
-function removeFromCart(cart, index) {
-  cart.items.splice(index.index, 1);
-
-  var total = 0;
-
-  cart.items.forEach(function(item) {
-    total += parseFloat(item.price);
-  });
-
-  cart.count = cart.items.length;
-  cart.total = total.toFixed(2);
-
-  return cart;
-}
-
-function updateCart(cart, newItem) {
-  cart.items.push(newItem);
-
-  var total = 0;
-
-  cart.items.forEach(function(item) {
-    total += parseFloat(item.price);
-  });
-
-  cart.count = cart.items.length;
-  cart.total = total.toFixed(2);
-
-  return cart;
-}
 
 function api(__env, $resource) {
   var baseUrl = __env.apiUrl;
